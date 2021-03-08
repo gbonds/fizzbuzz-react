5de4c8ac814c561dfbaca8aba7650b05212d2b3b
gbonds <ge.bonds@gmail.com>
2021-03-08 14:22:49 -0500

    Built out App with fizz buzz counting game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(1)
+  const fizzNumber = 3
+  const buzzNumber = 5
+
+  const addValue = () => {
+    setValue((value) => {
+      let newValue = value + 1
+      return newValue
+    })
+  }
+
+  const readValue = () => {
+    if ((Number.isInteger(value / fizzNumber)) && (Number.isInteger(value / buzzNumber))) {
+      return 'Fizz-Buzz'
+    }
+    if (Number.isInteger(value / fizzNumber)) {
+      return 'Fizz'
+    }
+    if (Number.isInteger(value / buzzNumber)) {
+      return 'Buzz'
+    }
+    else {
+      return value
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fizz Buzz Game</h1>
+        <p>Click the button below to begin</p>
+        <button onClick={addValue}>Count</button>
       </header>
+      <main className='App-content'>
+        <h2>
+          {readValue(value)}
+        </h2>
+      </main>
     </div>
   );
 }
